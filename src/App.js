ef427b267d66d69a5098e87c721224dff3d418b6
paristiffany <paristiffany12@gmail.com>
2024-02-16 08:41:22 +1100

    Enabled npm start for website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import appcss from "./App.module.css";
+
+import AboutUs from "./pages/AboutUs";
+import BookOnline from "./pages/BookOnline";
+import Firm from "./pages/Firm";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import Publications from "./pages/Publications";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import ContactUs from "./components/layout/ContactUs";
+import ChatWithUsButton from "./components/layout/ChatWithUsButton";
+import ChatBot from "./components/layout/chatbot-solicitors-website/chatbot-deployment/standalone-frontend/app";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={appcss.pagecontainer}>
+      <div className={appcss.contentwrap}>
+        <Navbar />
+        <Routes>
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/book-online" element={<BookOnline />} />
+          <Route path="/firm" element={<Firm />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/publications" element={<Publications />} />
+        </Routes>
+      </div>
+      <ChatBot />
+      <ChatWithUsButton />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
